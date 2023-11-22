ee6033a682097b5c0bdb0d18b479fa8219ca7dbf
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-11-22 02:10:16 +0200

    implement service interfaces

---
+++ backend/src/main/java/ua/knu/backend/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,65 @@
+package ua.knu.backend.service.impl;
+
+import ua.knu.backend.entity.Project;
+import ua.knu.backend.exception.project.ProjectByIdNotFoundException;
+import ua.knu.backend.exception.project.ProjectByNameNotFoundException;
+import ua.knu.backend.exception.project.ProjectWithTitleExistsException;
+import ua.knu.backend.repository.ProjectRepository;
+import ua.knu.backend.service.FolderService;
+import ua.knu.backend.service.ProjectService;
+
+import java.util.List;
+
+public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    private final FolderService folderService;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, FolderService folderService) {
+        this.projectRepository = projectRepository;
+        this.folderService = folderService;
+    }
+
+    @Override
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    @Override
+    public List<Project> getNotRequiredAuthenticationProjects() {
+        return projectRepository.getNotRequiredAuthenticationProjects();
+    }
+
+    @Override
+    public Project getProjectByTitle(String title) {
+        return projectRepository.findByTitle(title).orElseThrow(ProjectByNameNotFoundException::new);
+    }
+
+    @Override
+    public Project getProjectById(Integer id) {
+        return projectRepository.findById(id).orElseThrow(ProjectByIdNotFoundException::new);
+    }
+
+    @Override
+    public Project createProject(String title, boolean authenticated_only) {
+        throwExceptionIfProjectWithTitleMissing(title);
+        return projectRepository.save(new Project(title, authenticated_only));
+    }
+
+    @Override
+    public void deleteProject(Integer id) {
+        folderService.deleteFolder(getProjectById(id).getHiddenRootFolderId());
+        projectRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteProject(String title) {
+        deleteProject(getProjectByTitle(title).getId());
+    }
+
+    private void throwExceptionIfProjectWithTitleMissing(String title) {
+        if (projectRepository.findByTitle(title).isPresent())
+            throw new ProjectWithTitleExistsException(title);
+    }
+}
