10c6507cb61443d339a8e55762857dbef5c5242f
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-11-22 02:09:32 +0200

    update repository

--- backend/src/main/java/ua/knu/backend/repository/CodeSnippetRepository.java
+++ backend/src/main/java/ua/knu/backend/repository/CodeSnippetRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.knu.backend.entity.CodeSnippet;
 
 public interface CodeSnippetRepository extends JpaRepository<CodeSnippet, Integer> {
+
+    void deleteAllByFolderId(Integer folderId);
 }
