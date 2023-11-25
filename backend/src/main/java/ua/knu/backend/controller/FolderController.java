ccd5154f3af10f19e1a11f1546b9bfc97bd2cbcd
mykolaKhlopyk <mykola.khlopyk@gmail.com>
2023-11-25 02:44:31 +0200

    update controllers

--- backend/src/main/java/ua/knu/backend/controller/FolderController.java
+++ backend/src/main/java/ua/knu/backend/controller/FolderController.java
@@ -1,47 +1,44 @@
 package ua.knu.backend.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ua.knu.backend.entity.Folder;
 import ua.knu.backend.service.FolderService;
+import ua.knu.backend.web.dto.FolderDto;
+import ua.knu.backend.web.mapper.FolderMapper;
 
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 @RequestMapping("/folders")
-@RequiredArgsConstructor
 public class FolderController {
+
     private final FolderService folderService;
 
+    public FolderController(FolderService folderService) {
+        this.folderService = folderService;
+    }
+
     @GetMapping("/{id}")
-    public String getFolderById(@PathVariable Integer id) {
-        return folderService.getFolderById(id).toString();
+    public FolderDto getFolderById(@PathVariable Integer id) {
+        return FolderMapper.toDto(folderService.getFolderById(id));
     }
 
     @PostMapping("/create")
-    public String createFolder(@RequestParam Integer parentFolderId,
-                               @RequestParam String name){
-
-        return folderService.createFolder(parentFolderId, name).toString();
+    public FolderDto createFolder(@RequestParam Integer parentFolderId, @RequestParam String name) {
+        return FolderMapper.toDto(folderService.createFolder(parentFolderId, name));
     }
 
-    @DeleteMapping("/delete/{id}")
-    public String deleteFolder(@PathVariable Integer id){
-        return folderService.deleteFolder(id).toString();
+    @DeleteMapping("/{id}")
+    public FolderDto deleteFolder(@PathVariable Integer id) {
+        return FolderMapper.toDto(folderService.deleteFolder(id));
     }
 
-    @GetMapping("/{projectId}/root-folders")
-    public String getProjectRootFoldersById(@PathVariable Integer projectId){
-        return folderService.getProjectRootFolders(projectId).stream().map(Folder::toString).collect(Collectors.joining("\n"));
-    }
-
-    @GetMapping("/{projectTitle}/root-folders")
-    public String getProjectRootFoldersByTitle(@PathVariable Integer projectTitle){
-        return folderService.getProjectRootFolders(projectTitle).stream().map(Folder::toString).collect(Collectors.joining("\n"));
+    @GetMapping("/{projectId}/project-root-folders")
+    public List<FolderDto> getProjectRootFoldersById(@PathVariable Integer projectId) {
+        return folderService.getProjectRootFolders(projectId).stream().map(FolderMapper::toDto).toList();
     }
 
     @GetMapping("/{parentFolderId}/child-folders")
-    public String getChildFolders(@PathVariable Integer parentFolderId){
-        return folderService.getChildFolders(parentFolderId).stream().map(Folder::toString).collect(Collectors.joining("\n"));
+    public List<FolderDto> getChildFolders(@PathVariable Integer parentFolderId) {
+        return folderService.getChildFolders(parentFolderId).stream().map(FolderMapper::toDto).toList();
     }
 }
--- backend/src/main/java/ua/knu/backend/controller/ProjectController.java
+++ backend/src/main/java/ua/knu/backend/controller/ProjectController.java
@@ -1,55 +1,46 @@
 package ua.knu.backend.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ua.knu.backend.entity.Project;
 import ua.knu.backend.service.ProjectService;
+import ua.knu.backend.web.dto.ProjectDto;
+import ua.knu.backend.web.mapper.ProjectMapper;
 
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 @RequestMapping("/projects")
-@RequiredArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
 
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
     @GetMapping("/all")
-    public String getAllProjects() {
-        return projectService.getAllProjects().stream().map(Project::toString).collect(Collectors.joining("\n"));
+    public List<ProjectDto> getAllProjects() {
+        return projectService.getAllProjects().stream().map(ProjectMapper::toDto).toList();
     }
 
     @GetMapping("/not-required-authentication")
-    public String getNotRequiredAuthenticationProjects() {
-        return projectService.getNotRequiredAuthenticationProjects().stream().map(Project::toString).collect(Collectors.joining("\n"));
-    }
-
-    @GetMapping("/{title}")
-    public String getProjectByTitle(@PathVariable String title) {
-        return projectService.getProjectByTitle(title).toString();
+    public List<ProjectDto> getNotRequiredAuthenticationProjects() {
+        return projectService.getNotRequiredAuthenticationProjects().stream().map(ProjectMapper::toDto).toList();
     }
 
     @GetMapping("/{id}")
-    public String getProjectById(@PathVariable Integer id) {
-        return projectService.getProjectById(id).toString();
+    public ProjectDto getProjectById(@PathVariable Integer id) {
+        return ProjectMapper.toDto(projectService.getProjectById(id));
     }
 
     @PostMapping("/create")
-    public String createProject(
+    public ProjectDto createProject(
             @RequestParam String title,
             @RequestParam boolean authenticated_only
-    ){
-        return projectService.createProject(title, authenticated_only).toString();
+    ) {
+        return ProjectMapper.toDto(projectService.createProject(title, authenticated_only));
     }
 
-    @DeleteMapping("/delete/{id}")
-    public String deleteProjectById(@PathVariable Integer id){
+    @DeleteMapping("/{id}")
+    public void deleteProjectById(@PathVariable Integer id) {
         projectService.deleteProject(id);
-        return "200 OK";
-    }
-
-    @DeleteMapping("/delete/{title}")
-    public String deleteProjectByTitle(@PathVariable String title){
-        projectService.deleteProject(title);
-        return "200 OK";
     }
 }
