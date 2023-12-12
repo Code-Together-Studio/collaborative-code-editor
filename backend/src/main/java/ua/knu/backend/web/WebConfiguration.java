91b4b0c8b2e413b6a9bd0a53ee10c9c763bc9d50
TomGarfild <safronuyk.allex73@gmail.com>
2023-12-12 14:06:39 +0200

    Fix cors

--- backend/src/main/java/ua/knu/backend/web/WebConfiguration.java
+++ backend/src/main/java/ua/knu/backend/web/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
